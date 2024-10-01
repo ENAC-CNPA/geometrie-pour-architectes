32fe87ecbcab1997a69e405b5386ab9ddc9140e9
rvouilloz <97536689+rvouilloz@users.noreply.github.com>
2024-10-01 11:47:43 +0200

    update: vite.config.ts

--- vite-config.ts
+++ vite-config.ts
@@ -3,4 +3,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/geometrie-pour-architectes/",
+  esbuild: {
+    supported: {
+      "top-level-await": true,
+    },
+  },
 });
