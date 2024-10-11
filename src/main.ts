4d04aeec128da324c2f17f137e99d050153cf81f
rvouilloz <97536689+rvouilloz@users.noreply.github.com>
2024-10-11 11:24:59 +0200

    edit bernard model link

--- src/main.ts
+++ src/main.ts
@@ -42,13 +42,14 @@ async function main() {
 
   /** Create a loader for the speckle stream */
   const versionsUrls: string[] = [
-    "75e4788d3e@58fb90f03e",
-    //"75e4788d3e@a815b298c9",
-    //"75e4788d3e@e4414e9ea1",
+    //"75e4788d3e@58fb90f03e", //dev model version 1
+    //"75e4788d3e@a815b298c9", //dev model version 2
+    //"75e4788d3e@e4414e9ea1", //dev model version 3
+    "a3ad727b4a@adce166f4a", //bernard model version 1
   ];
   const urls = await UrlHelper.getResourceUrls(
     //"https://app.speckle.systems/projects/d5b671524f/models/" + //dev model
-    "https://app.speckle.systems/projects/0b4aee874b/models/" +
+    "https://app.speckle.systems/projects/0b4aee874b/models/" + //bernard model
       versionsUrls.join(",")
   );
   for (const url of urls) {
