2401f0e3ad0834dd029f88b5648e2670da5cc5fd
rvouilloz <97536689+rvouilloz@users.noreply.github.com>
2026-01-05 10:09:18 +0100

    edit model url

--- src/extensions/models.ts
+++ src/extensions/models.ts
@@ -1,7 +1,7 @@
 export const models = [
     {
         name: "Newton: Principia, I,5: Lemma 22 and Propositions XXV and XXVI. Model by Bernard Cache",
-        speckleUrl: "5626cd6127@c377ad44cb"
+        speckleUrl: "5626cd6127@20f8073b6b"
     },{
         name: "Derand: Voûte d'arêtes en tour ronde et rampante. Model by Raphaël Vouilloz",
         speckleUrl: "5626cd6127@790b98d48b"
