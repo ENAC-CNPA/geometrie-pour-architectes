0f0f22edbade00b70de973f20f635d4da4282dbd
rvouilloz <97536689+rvouilloz@users.noreply.github.com>
2026-05-12 15:13:44 +0200

    edit vite.config.ts for index_ieee2026

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: "index.html",
+        main_ieee2026: "index_ieee2026.html",
         viewer: "viewer.html",
       },
     },
